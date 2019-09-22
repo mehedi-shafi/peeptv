d17977cede99d3f983cdcfc050d93db957f7f1cf
root <mehedishafi@codemarshal.com>
2019-09-22 13:08:19 +0600

    feat: default routing path return function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tvscraper import tvscraper
 
 tvbox = tvscraper()
 
-@app.route('/stream-link/<channel_name>')
+@app.route('/stream-link/<channel_name>', methods=['GET', 'POST'])
 def get_channel_stream_link(channel_name):
     stream_link = tvbox.get_stream_link(channel_name)
     return jsonify(stream_link)
@@ -15,6 +15,11 @@ def get_channel_stream_link(channel_name):
 @app.route('/channel-list', methods=['GET'])
 def get_channel_list():
     return jsonify(tvbox.get_channels_dict())
+
+@app.route('/', methods=['GET'])
+def landing():
+    with open('README.md', 'r') as file:
+        return file.read()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
